2cb00cdcd5235764060b087bf6a98f0134076230
litleaf <litleaf@gmail.com>
2013-07-10 07:22:33 -0700

    hw3-1

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var home = fs.readFileSync('index.html');
+  response.send(home.toString());
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
